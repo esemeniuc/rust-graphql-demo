e1b296f0a8f9342e88881d4dca137ec24ab7e21f
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-03-19 13:35:51 -0700

    add basic demo

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![feature(decl_macro, proc_macro_hygiene)]
+extern crate juniper;
+
+use rocket::{response::content, State};
+
+mod graphql;
+
+#[rocket::get("/")]
+fn graphiql() -> content::Html<String> {
+    juniper_rocket::playground_source("/graphql")
+}
+
+#[rocket::get("/graphql?<request>")]
+fn get_graphql_handler(
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<graphql::Schema>,
+    context: State<()>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
+#[rocket::post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<graphql::Schema>,
+    context: State<()>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
+fn main() {
+    rocket::ignite()
+        .manage(graphql::create_schema())
+        .manage(())
+        .mount(
+            "/",
+            rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
+        )
+        .launch();
+}
